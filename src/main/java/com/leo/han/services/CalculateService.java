82a0ae7b5bd85ca3a0d51608086b96b93fd59888
iamleohandev <iamleohandev@gmail.com>
2014-06-21 16:50:29 -0400

    first commit

---
+++ src/main/java/com/leo/han/services/CalculateService.java
@@ -0,0 +1,12 @@
+package com.leo.han.services;
+
+public interface CalculateService {
+
+	
+	String getSummary();
+	
+	int sum(int a, int b);
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/jsps/home.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome home</h2>
+</body>
+</html>
